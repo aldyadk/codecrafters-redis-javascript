0fc9e4f26ddaf69dd663474555a1745b8d06b396
Aldy Andika <aldy.andika@gmail.com>
2024-07-13 15:05:15 +0000

    handle PONG

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+const { Socket } = require("dgram");
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -5,7 +6,14 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on('close', ()=>{
+    connection.end()
+  })
+  connection.on('data',(data)=>{
+      console.log(data)
+      connection.write('+PONG\r\n')
+  })
 });
 
 server.listen(6379, "127.0.0.1");
+
